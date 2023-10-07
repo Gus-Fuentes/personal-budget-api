00f34dd15ca4d537718e12f91b8d25bb0b30020e
Gustavo Fuentes <5453tavo@gmail.com>
2023-10-07 18:45:57 +0200

    Add 'id' system

--- global.js
+++ global.js
@@ -1,4 +1,5 @@
 let envelopes = [];
 let totalBudget = 0;
+let id = 1;
 
-export default {envelopes, totalBudget};
+export default {envelopes, totalBudget, id};
--- routes/generate.js
+++ routes/generate.js
@@ -3,29 +3,19 @@ import express, { Router } from "express"
 
 const generateRoute = Router()
 
-// const betterRequest = (req, res, next)=>{
-//     console.log(req.body)
-    
-//     if(Object.keys(req.body).length === 0){
-//         next()
-//     }else{
-//         for (let key in req.body){
-//             console.log(key);
-//         }
-
-//         next()
-//     }
-// }
-
 generateRoute.use(express.json())
 
 generateRoute.post('/', (req, res, next)=>{
+    //add an ID to the envelope
+    req.body.envelope.id = global.id;
+    global.id++
+
     if(req.body.envelope){
         global.envelopes.push(req.body.envelope)
     }
     console.log(req.body.envelope)
 
-    res.send("hola");
+    res.send(global.envelopes.filter((env)=>env.id===req.body.envelope.id));
 })
 
 
