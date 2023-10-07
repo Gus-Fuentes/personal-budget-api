b1b353530f35b6c75690d18c9bab21cb4ccc9ea4
Gustavo Fuentes <5453tavo@gmail.com>
2023-10-07 23:53:25 +0200

    Add retrieving function by ID

--- routes/envelopes.js
+++ routes/envelopes.js
@@ -1,11 +1,17 @@
 import global from "../global.js"
-import express, { Router } from "express"
+import { Router } from "express"
 
 const envelopeRoute = Router()
 
-envelopeRoute.get("/", (req, res, next)=>{
-    console.log(global.envelopes)
-    res.send(global.envelopes)
+envelopeRoute.get("/", (req, res, next) => {
+    console.log(global.envelopes);
+    res.send(global.envelopes);
 })
 
-export default {envelopeRoute}
+envelopeRoute.get("/:id", (req, res, next) => {
+    const id = Number(req.params.id)
+    const envelope = global.envelopes.find(env => env.id === id);
+    res.send(envelope);
+})
+
+export default { envelopeRoute };
