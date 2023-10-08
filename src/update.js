fe4c6f1ec9f3d915ad6f156243f90e1ac55b779f
Gustavo Fuentes <5453tavo@gmail.com>
2023-10-08 15:16:45 +0200

    Fix PUT /envelopes/update bug

--- src/update.js
+++ src/update.js
@@ -6,11 +6,16 @@ const updateRoute = express.Router()
 updateRoute.use(express.json())
 
 updateRoute.put("/", global.envDataValidation, (req, res, next)=>{ // Update envelope by id
-    const id = Number(req.params.id);
+    const id = Number(req.body.envelope.id);
     const name = req.body.envelope.name;
     const budget = req.body.envelope.budget;
 
+    console.log(id);
+    console.log(typeof id)
+    console.log(name);
+
     const indexEnvToChange = global.envelopes.findIndex(env=>env.id === id);
+    console.log(indexEnvToChange)
     global.envelopes[indexEnvToChange].name = name
     global.envelopes[indexEnvToChange].budget = budget
 
