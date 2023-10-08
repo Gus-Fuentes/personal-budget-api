fc3befefa64b57c2e972d8dd8e6d28349a138bd6
Gustavo Fuentes <5453tavo@gmail.com>
2023-10-08 11:51:45 +0200

    Add PUT /envelope/update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import generate from "./src/generate.js";
 import envelopes from "./src/envelopes.js";
+import update from "./src/update.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,6 +9,7 @@ const app = express()
 
 app.use("/envelopes/generate", generate.generateRoute)
 app.use("/envelopes", envelopes.envelopeRoute)
+app.use("/envelopes/update", update.updateRoute)
 
 app.get('/', (req, res, next)=>{
     res.send('Welcome to Personal Budget');
--- src/update.js
+++ src/update.js
@@ -5,6 +5,17 @@ const updateRoute = express.Router()
 
 updateRoute.use(express.json())
 
-updateRoute.put("/:id", envDataValidation, (req, res, next)=>{
-    const id = Number(req.params.id)
+updateRoute.put("/", global.envDataValidation, (req, res, next)=>{ // Update envelope by id
+    const id = Number(req.params.id);
+    const name = req.body.envelope.name;
+    const budget = req.body.envelope.budget;
+
+    const indexEnvToChange = global.envelopes.findIndex(env=>env.id === id);
+    global.envelopes[indexEnvToChange].name = name
+    global.envelopes[indexEnvToChange].budget = budget
+
+    res.json(global.envelopes[indexEnvToChange])
+
 })
+
+export default {updateRoute};
